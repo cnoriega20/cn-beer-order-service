da662da6a19a712036affe42141f80b12605c4a8
cnoriega20 <c79.noriega@gmail.com>
2020-10-03 20:18:19 -0500

    Spring Intialzr project creation.

---
+++ src/main/java/tn/msvcs/cnbeerorderservice/CnBeerOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.msvcs.cnbeerorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnBeerOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnBeerOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/msvcs/cnbeerorderservice/CnBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.msvcs.cnbeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnBeerOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
